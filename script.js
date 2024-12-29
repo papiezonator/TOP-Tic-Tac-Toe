261ad26aabc9307aad845fc0704bb60a810e8856
papiezonator <52249311+papiezonator@users.noreply.github.com>
2024-12-29 22:47:43 +0100

    players markers now can be added to array

--- script.js
+++ script.js
@@ -17,14 +17,18 @@ function gameBoard() {
     console.log(board);
 }
 
+//checking game's status
+
 function gameStatus(){
     for(let i = 0; i < board.length; i++){
         console.log(board[i]);
-        for(let j = 0; j < board[i].length; j++){
+        /*for(let j = 0; j < board[i].length; j++){
             console.log(board[i][j]);
-        }
+        }*/
     }
 }
+
+//player variables
 
 const playerOne = {
     name: "tim",
@@ -34,4 +38,24 @@ const playerOne = {
 const playerTwo = {
     name: "jenn",
     mark: "o"
+}
+
+function gameplay(){
+    let state;
+    let row1 = board[0];
+    let row2 = board[1];
+    let row3 = board[2];
+    while(state != "finished"){
+        let test = prompt("x?");
+        let row = prompt("which row?");
+        let column = prompt("which column?")
+        if(test === 'x'){
+            board[row-1].splice(column-1, 1, test);
+            state = 'finished';
+            console.log(playerOne.mark, playerTwo.mark); 
+            gameStatus();
+            return;
+        }   
+    }
+
 }
