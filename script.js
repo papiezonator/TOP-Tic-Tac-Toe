aebead009d1e1c3a2283dc55cec2b4e1cae6b097
papiezonator <52249311+papiezonator@users.noreply.github.com>
2024-12-31 19:43:37 +0100

    added a starting player system

--- script.js
+++ script.js
@@ -38,24 +38,46 @@ const playerOne = {
 const playerTwo = {
     name: "jenn",
     mark: "o"
-}
+};
 
 function gameplay(){
     let state;
+    let firstPlayer
+    let secondPlayer
     let row1 = board[0];
     let row2 = board[1];
     let row3 = board[2];
     while(state != "finished"){
-        let test = prompt("x?");
-        let row = prompt("which row?");
-        let column = prompt("which column?")
-        if(test === 'x'){
+        //choosing the starting player
+        while(firstPlayer != playerOne.name && firstPlayer != playerTwo.name){
+            firstPlayer = prompt("Who will start the game?");  
+        }
+        if(firstPlayer === playerOne.name){
+            firstPlayer = playerOne;
+            secondPlayer = playerTwo;
+        } else {
+            firstPlayer = playerTwo;
+            secondPlayer = playerOne;
+        }
+
+        console.log(`The first player is: ${firstPlayer.name}, the second one: ${secondPlayer.name}. Enjoy your game!`);
+        console.log(`Your marks are: ${firstPlayer.name}, ${firstPlayer.mark} and ${secondPlayer.name}, ${secondPlayer.mark}`);
+
+        state = 'finished';
+        /*test = x;
+        {
+            let row = prompt(`${test}, which row?`);
+            let column = prompt(`${test}, which column?`);
+            if(test.mark === 'x'){
             board[row-1].splice(column-1, 1, test);
             state = 'finished';
             console.log(playerOne.mark, playerTwo.mark); 
             gameStatus();
             return;
-        }   
+        }
+        
+        }
+        */   
     }
 
 }
