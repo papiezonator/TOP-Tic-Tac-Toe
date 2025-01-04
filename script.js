70413c53b956f929efef58c047517efe2175d90d
papiezonator <52249311+papiezonator@users.noreply.github.com>
2025-01-04 21:35:56 +0100

    adjusted variables within gameplay f, added a welcoming message

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ document.body.onload = gameBoard;
 
 let board = [];
 
-
-
 function gameBoard() {
     //used variables
     const div = document.createElement("div");
@@ -71,13 +69,85 @@ const playerTwo = {
 
 
 function gameplay(){
+    let div = document.createElement("div");
+    let h1 = document.createElement("h1");
+    let tableWrapper = document.querySelector(".tableWrapper");
+    div.appendChild(h1);
+    div.className = "welcomeMessage";
+    document.body.insertAdjacentElement("beforebegin", div);
+    
+    let firstPlayer = playerOne;
+    let secondPlayer = playerTwo;
+    h1.innerHTML = `Welcome ${playerOne.name} and ${playerTwo.name}!`;
+
     let items = document.querySelectorAll(".item");
     for(let i = 0; i < items.length; i++){
         items[i].addEventListener('click', () => {
             console.log(items[i].innerHTML);
+            console.log(`${playerOne.mark}, ${playerTwo.mark}`)
         })
     }
     
+
+    /*let state;
+    let row1 = board[0];
+    let row2 = board[1];
+    let row3 = board[2];
+    
+    //choosing the starting player
+
+    while(firstPlayer != playerOne.name && firstPlayer != playerTwo.name){
+        firstPlayer = prompt("Who will start the game?");  
+    }
+    if(firstPlayer === playerOne.name){
+        firstPlayer = playerOne;
+        secondPlayer = playerTwo;
+    } else {
+        firstPlayer = playerTwo;
+        secondPlayer = playerOne;
+    }
+
+    console.log(`The first player is: ${firstPlayer.name}, the second one: ${secondPlayer.name}. Enjoy your game!`);
+    console.log(`Your marks are: ${firstPlayer.name}, ${firstPlayer.mark} and ${secondPlayer.name}, ${secondPlayer.mark}`);
+    
+    //the actual gameplay
+
+    while(state != "finished"){
+    
+        //rounds
+        let roundNumber = 0;
+        rounds(firstPlayer);
+        rounds(secondPlayer);
+        roundNumber++;
+        console.log(`Round number ${roundNumber} result:`);
+        gameStatus();
+
+        state = 'finished';
+        }*/
+          
+    /*}
+    
+    function rounds(currentPlayer){
+        
+        let row;
+        let column; 
+        let correctPosition;
+        while(correctPosition != true){
+            row = prompt(`${currentPlayer.name}, please choose a row`);
+            column = prompt(`${currentPlayer.name}, please choose a column`);
+            if(board[row-1][column-1] != ''){
+                console.log("This spot is already marked, please choose a different one");
+                gameStatus();
+                correctPosition = false;
+            } else {
+                correctPosition = true;
+            }
+        }
+        
+        board[row-1].splice(column-1, 1, currentPlayer.mark);
+        gameStatus();
+        console.log("");
+    */ 
 }
 
     
